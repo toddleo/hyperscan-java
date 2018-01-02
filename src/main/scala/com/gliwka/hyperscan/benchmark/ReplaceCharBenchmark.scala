e3f99e38b7e028ca73226d4684c49a408d2c46c6
todd.leo <todd.f.leo@gmail.com>
2018-01-02 02:35:13 -0500

    refactor: mark test class as @State, and use @Setup to set fixtures properly

--- src/main/scala/com/gliwka/hyperscan/benchmark/ReplaceCharBenchmark.scala
+++ src/main/scala/com/gliwka/hyperscan/benchmark/ReplaceCharBenchmark.scala
@@ -12,28 +12,32 @@ case class ScannerAndDB(scanner: Scanner, db: Database)
 /**
   * This benchmark simply generate UUIDs and replace all digits to character 'd'
   */
+@State(Scope.Benchmark)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @BenchmarkMode(Array(Mode.Throughput))
 class ReplaceCharBenchmark {
+
+  val pattern: String = """\d"""
+  var expr: Expression = _
+  var db: Database = _
+  var scanner: Scanner = _
+
+  @Setup(Level.Trial)
+  def Fixture(): Unit = {
+    expr = new Expression(pattern, util.EnumSet.of(ExpressionFlag.SOM_LEFTMOST))
+    db = Database.compile(expr)
+    scanner = new Scanner()
+    scanner.allocScratch(db)
+  }
 
   /**
     * Replace digits via HyperScan, with pre-build regex
     */
   @Benchmark
   def HyperScan(): Unit = {
-    val scannerAndDB: ScannerAndDB = Compile("""\d""")
-    val (scanner, db) = scannerAndDB.scanner -> scannerAndDB.db
     val str = java.util.UUID.randomUUID.toString
     val strBld = new StringBuilder(str)
     HyperScanReplace(matcheItr = scanner.scan(db, str).iterator.toIterator, strBld = strBld, replacement = "d")
-  }
-
-  private def Compile(pattern: String) = {
-    val expr: Expression = new Expression(pattern, util.EnumSet.of(ExpressionFlag.SOM_LEFTMOST))
-    val db = Database.compile(expr)
-    val scanner = new Scanner()
-    scanner.allocScratch(db)
-    ScannerAndDB(scanner, db)
   }
 
   private def HyperScanReplace(matcheItr: Iterator[Match], strBld: StringBuilder, replacement: String): StringBuilder = {
@@ -48,8 +52,5 @@ class ReplaceCharBenchmark {
     * Replace digits via native Java Regex
     */
   @Benchmark
-  def Native: Unit = {
-    val pattern: String = """\d"""
-    java.util.UUID.randomUUID.toString.replaceAll(pattern, "d")
-  }
+  def Native(): Unit = java.util.UUID.randomUUID.toString.replaceAll(pattern, "d")
 }
